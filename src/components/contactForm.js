905496b83bc300dd4c8fad46cf9593be5243e787
Nhat Bui <jakenhat@gmail.com>
2022-06-15 11:58:36 +0700

    google form ok

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -36,27 +36,33 @@ class ContactForm extends Component {
     //   message: '',
     //   submit:true
     // });
-    const scriptUrl = "https://script.google.com/macros/s/AKfycbwCqWtnagFc_8hJJCWSVRRmD1SHYYRBRyD9DXId-uE0HUQ7exwGlOebd3ZAIzTmiTzenw/exec"
+    const scriptUrl = "https://sheet.best/api/sheets/3f7642d3-dd05-4f37-a997-89c7ac330084"
 
   const item = {
+      Timestamp: new Intl.DateTimeFormat('en-UK', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(Date.now()),
       name: this.state.name,
       email: this.state.email,
       message: this.state.message
     }
-    fetch(scriptUrl, {
-    method: 'POST', 
-    body: new FormData(event.target.value),
+    // fetch(scriptUrl, {
+    // method: 'POST', 
+    // body: new FormData(event.target.value),
 
-    }).then(res => {
-          console.log(res)
-          console.log("SUCCESSFULLY SUBMITTED")
-          // setLoading(false)
-    })
-      .catch(err => console.log(err))
+    // }).then(res => {
+    //       console.log(res)
+    //       console.log("SUCCESSFULLY SUBMITTED")
+    //       // setLoading(false)
+    // })
+    //   .catch(err => console.log(err))
     // axios.post(scriptUrl, this.state)
     // .then(response => {
     //   console.log(response);
     // })
+
+    axios.post(scriptUrl, item)
+    .then((response) => {
+      console.log("ok");
+    })
     this.setState({
         name: '',
         email: '',
